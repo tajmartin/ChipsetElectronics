a27ddc0127a143642e5d703350f9804ef7037403
tmartin <tmartin@tajmartin.com>
2015-07-30 16:50:02 -0400

    Initial commit.

---
+++ includes/functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: odin
+ * Date: 7/30/15
+ * Time: 4:42 PM
+ */
+
+function redirectTo($link) {
+    header("Location: " . $link);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: odin
+ * Date: 7/30/15
+ * Time: 4:41 PM
+ */
+
